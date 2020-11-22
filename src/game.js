e82684c28baf587e5f159e789a1d8eedeb3ec638
Ellen Granoff <granoff.ellen@gmail.com>
2020-11-22 12:40:29 -0500

    Feature: Snake head moves

--- src/game.js
+++ src/game.js
@@ -2,15 +2,9 @@ import Phaser from "phaser";
 
 const config = {
   type: Phaser.AUTO,
-  width: 900,
-  height: 600,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { x: 0, y: 0 },
-      debug: false,
-    },
-  },
+  width: 888,
+  height: 500,
+  backgroundColor: "#F03B21",
   scene: {
     preload: preload,
     create: create,
@@ -18,33 +12,136 @@ const config = {
   },
 };
 
-let head, tail, cursors, snake, toiletpaper, playerDirection;
-const directions = {
-  up: 0,
-  down: 1,
-  left: 2,
-  right: 3,
-};
+let snake;
+let cursors;
 
-const game = new Phaser.Game(config);
+//  Direction consts
+const UP = 0;
+const DOWN = 1;
+const LEFT = 2;
+const RIGHT = 3;
+
+var game = new Phaser.Game(config);
 
 function preload() {
+  this.load.image("food", "assets/toiletpaper.png");
   this.load.image("map", "assets/usmap.jpeg");
-  this.load.image("maskface", "assets/maskface.png");
-  this.load.image("toiletpaper", "assets/toiletpaper.png");
+  this.load.image("face", "assets/maskface.png");
 }
 
 function create() {
-  this.add.image(400, 300, "map").setScale(1);
-  this.add.image(40, 40, "maskface").setScale(0.15);
+  this.add.image(444, 250, "map").setScale(0.8);
+  var Snake = new Phaser.Class({
+    initialize: function Snake(scene, x, y) {
+      this.headPosition = new Phaser.Geom.Point(x, y);
+      this.body = scene.add.group();
+      this.head = this.body.create(x * 30, y * 30, "face").setScale(0.11);
+      this.head.setOrigin(0);
+      this.alive = true;
+      this.speed = 150;
+      this.moveTime = 0;
+      this.heading = RIGHT;
+      this.direction = RIGHT;
+    },
 
-  cursors = game.input.keyboard.createCursorKeys();
+    update: function (time) {
+      if (time >= this.moveTime) {
+        return this.move(time);
+      }
+    },
+
+    faceLeft: function () {
+      if (this.direction === UP || this.direction === DOWN) {
+        this.heading = LEFT;
+      }
+    },
+
+    faceRight: function () {
+      if (this.direction === UP || this.direction === DOWN) {
+        this.heading = RIGHT;
+      }
+    },
+
+    faceUp: function () {
+      if (this.direction === LEFT || this.direction === RIGHT) {
+        this.heading = UP;
+      }
+    },
+
+    faceDown: function () {
+      if (this.direction === LEFT || this.direction === RIGHT) {
+        this.heading = DOWN;
+      }
+    },
+
+    move: function (time) {
+      switch (this.heading) {
+        case LEFT:
+          this.headPosition.x = Phaser.Math.Wrap(
+            this.headPosition.x - 1,
+            0,
+            55
+          );
+          break;
+
+        case RIGHT:
+          this.headPosition.x = Phaser.Math.Wrap(
+            this.headPosition.x + 1,
+            0,
+            55
+          );
+          break;
+
+        case UP:
+          this.headPosition.y = Phaser.Math.Wrap(
+            this.headPosition.y - 1,
+            0,
+            30
+          );
+          break;
+
+        case DOWN:
+          this.headPosition.y = Phaser.Math.Wrap(
+            this.headPosition.y + 1,
+            0,
+            30
+          );
+          break;
+      }
+
+      this.direction = this.heading;
+
+      Phaser.Actions.ShiftPosition(
+        this.body.getChildren(),
+        this.headPosition.x * 16,
+        this.headPosition.y * 16,
+        1
+      );
+
+      this.moveTime = time + this.speed;
+
+      return true;
+    },
+  });
+
+  snake = new Snake(this, 0, 0);
+
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
-function newSnakeHead(x, y) {
-  let newHead = {};
-  newHead.image = game.add.image(x, y, "maskface");
-  newHead.next = null;
-}
+function update(time, delta) {
+  if (!snake.alive) {
+    return;
+  }
+  if (cursors.left.isDown) {
+    snake.faceLeft();
+  } else if (cursors.right.isDown) {
+    snake.faceRight();
+  } else if (cursors.up.isDown) {
+    snake.faceUp();
+  } else if (cursors.down.isDown) {
+    snake.faceDown();
+  }
 
-function update(time, delta) {}
+  snake.update(time);
+}
